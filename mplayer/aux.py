c522fef01cdd8a017d2c4ca2add45ae89ee6a4cf
Bing Sun <subi.the.dream.walker@gmail.com>
2013-02-09 15:24:17 +0800

    mplayer/aux.py: move find_more_episode() in.

--- mplayer/aux.py
+++ mplayer/aux.py
@@ -1,8 +1,10 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
 # Copyright 2010-2013 Bing Sun <subi.the.dream.walker@gmail.com>
-# Time-stamp: <2013-01-12 19:03:01 by subi>
+# Time-stamp: <2013-02-09 15:23:16 by subi>
+
+from __future__ import unicode_literals
 
 import os
 
@@ -23,3 +25,61 @@ def which(prog):
           return fullpath
     return None
 
+def find_more_episodes(filepath):
+    '''Try to find some following episodes/parts.
+    '''
+    def translate(s):
+        dic = dict(zip('零壹贰叁肆伍陆柒捌玖〇一二三四五六七八九','0123456789'*2))
+        return ''.join([dic.get(c,c) for c in s])
+    def split_by_int(s):
+        res = [(int(x) if x.isdigit() else -len(x)) for x in re.split('(\d+)', translate(s)) if x != '']
+        return res
+    def strip_to_int(s,prefix):
+        # strip the prefix
+        if prefix and s.startswith(prefix):
+            _,_,s = s.partition(prefix)
+        # extract the first int
+        val = split_by_int(s)[0]
+        return val
+
+    if not os.path.exists(filepath):
+        return []
+
+    import re
+    pdir, basename = os.path.split(os.path.abspath(filepath))
+    _, ext = os.path.splitext(basename)
+    # basic candidate filtering
+    # 1. extention
+    files = [f for f in os.listdir(pdir) if f.endswith(ext)]
+    # 2. remove previous episodes
+    files.sort(key=split_by_int)
+    del files[0:files.index(basename)]
+
+    # not necessary to go further if no candidates
+    if len(files) == 1:
+        return []
+
+    # find the common prefix
+    i_break = 0
+    for i in range(min(len(files[0]),len(files[1]))):
+        if not files[0][i] == files[1][i]:
+           i_break = i
+           break
+    prefix = files[0][0:i_break]
+
+    # generate the list
+    results = []
+    for i,f in enumerate(files[1:]):
+        if strip_to_int(f,prefix) - strip_to_int(files[i],prefix) == 1:
+            results.append(os.path.join(pdir,f))
+        else:
+            break
+    return results
+
+if __name__ == '__main__':
+    import sys
+    if len(sys.argv) != 1:
+        if sys.hexversion < 0x03000000:
+            print('\n'.join(find_more_episodes(sys.argv[1].decode(sys.getfilesystemencoding()))))
+        else:
+            print('\n'.join(find_more_episodes(sys.argv[1])))
